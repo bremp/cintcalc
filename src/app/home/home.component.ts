c00cd35217e0e839c61b1c985ae9ca3e3ec70710
Jayneson <jayneson@gmail.com>
2018-09-05 18:00:35 -1000

    Implemented onChange event handler.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, FormBuilder, Validators } from '@angular/forms';
-import {CurrencyPipe} from '@angular/common';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, AbstractControl } from '@angular/forms';
+import { CurrencyPipe } from '@angular/common';
+
+import { Subject } from 'rxjs';
+import { debounceTime, takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
+  // Constant
+  private readonly calcDelay: number = 1000;
+  private readonly percent: number = 100;
+  private readonly calendarDays: number = 365;
 
   form: FormGroup;
+  limitControl: AbstractControl;
+  minPaymentRateControl: AbstractControl;
+  minPaymentControl: AbstractControl;
+  aprControl: AbstractControl;
+  dailyInterestRateControl: AbstractControl;
+  monthlyBalanceControl: AbstractControl;
+  daysInMonthControl: AbstractControl;
+  monthlyInterestChargeControl: AbstractControl;
+  monthlyPrincipalPaymentControl: AbstractControl;
+
+  private unsubscribe = new Subject<void>();  // variable to unsubscribe from valueChanges observables.
 
   constructor(private fb: FormBuilder, private currencyPipe: CurrencyPipe) { }
 
@@ -18,7 +36,6 @@ export class HomeComponent implements OnInit {
     const defaultMinPaymentRate = 0.659;
     const defaultMonthlyBalance = 96597.87;
     const defaultApr = 2.75;
-    const calendarDays = 365;
     const numberPattern = '^\\d*\.?\\d+$';
     const currencyPattern = '^[0-9]{1,3}(?:,?[0-9]{3})*(?:\.[0-9]{2})?$';
 
@@ -29,35 +46,89 @@ export class HomeComponent implements OnInit {
       minPaymentRate: [defaultMinPaymentRate,
         [Validators.required, Validators.pattern(numberPattern)]
       ],
-      minPayment: {value: (defaultLimit * (defaultMinPaymentRate / 100)) , disabled: true},
+      minPayment: { value: (defaultLimit * (defaultMinPaymentRate / this.percent)), disabled: true },
       apr: [defaultApr,
         [Validators.required, Validators.pattern(numberPattern)]
       ],
-      dailyInterestRate: {value: (defaultApr / 100 / calendarDays), disabled: true},
+      dailyInterestRate: { value: (defaultApr / this.percent / this.calendarDays), disabled: true },
       monthlyBalance: [defaultMonthlyBalance,
         [Validators.required, Validators.pattern(currencyPattern)]
       ],
       daysInMonth: [30,
         [Validators.required, Validators.pattern(numberPattern)]
       ],
-      monthlyInterestCharge: {value: '?', disabled: true},
-      monthlyPrincipalPayment: {value: '?', disabled: true}
+      monthlyInterestCharge: { value: '?', disabled: true },
+      monthlyPrincipalPayment: { value: '?', disabled: true }
     });
+
+    this.getFormControls();
+    this.onChanges();
+  }
+
+  private getFormControls() {
+    this.limitControl = this.form.get('limit');
+    this.minPaymentRateControl = this.form.get('minPaymentRate');
+    this.minPaymentControl = this.form.get('minPayment');
+    this.aprControl = this.form.get('apr');
+    this.dailyInterestRateControl = this.form.get('dailyInterestRate');
+    this.monthlyBalanceControl = this.form.get('monthlyBalance');
+    this.daysInMonthControl = this.form.get('daysInMonth');
+    this.monthlyInterestChargeControl = this.form.get('monthlyInterestCharge');
+    this.monthlyPrincipalPaymentControl = this.form.get('monthlyPrincipalPayment');
+  }
+
+  ngOnDestroy() {
+    this.unsubscribe.next();
+    this.unsubscribe.complete();
   }
 
   calculate() {
-      const minPaymentControl = this.form.get('minPayment');
-      const monthlyBalanceControl = this.form.get('monthlyBalance');
-      const dailyInterestRateControl = this.form.get('dailyInterestRate');
-      const daysInMonthControl = this.form.get('daysInMonth');
-      const monthlyInterestChargeControl = this.form.get('monthlyInterestCharge');
-      const monthlyPrincipalPaymentControl = this.form.get('monthlyPrincipalPayment');
+    const interestCharge = this.monthlyBalanceControl.value * this.dailyInterestRateControl.value * this.daysInMonthControl.value;
+    this.monthlyInterestChargeControl.setValue(this.currencyPipe.transform(interestCharge, 'USD', '', '1.2-2'));
 
-      const interestCharge = monthlyBalanceControl.value * dailyInterestRateControl.value * daysInMonthControl.value;
-      monthlyInterestChargeControl.setValue(this.currencyPipe.transform(interestCharge, 'USD', '', '1.2-2'));
+    const principalPayment = this.minPaymentControl.value - interestCharge;
+    this.monthlyPrincipalPaymentControl.setValue(this.currencyPipe.transform(principalPayment, 'USD', '', '1.2-2'));
+  }
 
-      const principalPayment = minPaymentControl.value - interestCharge;
-      monthlyPrincipalPaymentControl.setValue(this.currencyPipe.transform(principalPayment, 'USD', '', '1.2-2'));
+  private onChanges() {
+    // Auto calculate ROI when noi or purchasePrice changes.
+    let minPayment = 0;
+    this.limitControl.valueChanges
+      .pipe(
+        debounceTime(this.calcDelay),
+        takeUntil(this.unsubscribe)
+      )
+      .subscribe(val => {
+        if (this.limitControl.valid && this.minPaymentRateControl) {
+          minPayment = val * (this.minPaymentRateControl.value / this.percent);
+        }
+        this.minPaymentControl.setValue(minPayment);
+      });
+
+    this.minPaymentRateControl.valueChanges
+      .pipe(
+        debounceTime(this.calcDelay),
+        takeUntil(this.unsubscribe)
+      )
+      .subscribe(val => {
+        if (this.limitControl.valid && this.minPaymentRateControl.valid) {
+          minPayment = this.limitControl.value * (val / this.percent);
+        }
+        this.minPaymentControl.setValue(minPayment);
+      });
+
+    let dailyInterest = 0;
+    this.aprControl.valueChanges
+    .pipe(
+      debounceTime(this.calcDelay),
+      takeUntil(this.unsubscribe)
+    )
+    .subscribe(val => {
+      if (this.aprControl.valid) {
+        dailyInterest = val / this.percent / this.calendarDays;
+      }
+      this.dailyInterestRateControl.setValue(dailyInterest);
+    });
   }
 
 }
